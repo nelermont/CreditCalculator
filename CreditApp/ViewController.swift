203af678500e45f7820c285196725f35d3ae7f8f
nelermont <nelermont@yandex.ru>
2020-11-10 10:39:38 +0300

    Initial Commit

---
+++ CreditApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreditApp
+//
+//  Created by Дмитрий Подольский on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
